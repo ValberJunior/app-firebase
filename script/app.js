eb3d62ec2ebb69b39d4bd8c7c13023d9e462a962
Valber Junior <84424883+ValberJunior@users.noreply.github.com>
2021-08-25 10:46:18 -0300

    Update

--- script/app.js
+++ script/app.js
@@ -1,22 +1,24 @@
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCGRMQ0BuiKB-wCZuxyhTyEJQGW_86ARro",
     authDomain: "treinoapp-937bf.firebaseapp.com",
+    databaseURL: "https://treinoapp-937bf-default-rtdb.firebaseio.com",
     projectId: "treinoapp-937bf",
     storageBucket: "treinoapp-937bf.appspot.com",
     messagingSenderId: "792663491656",
     appId: "1:792663491656:web:2cd35889a2acc4bbcdc47d",
     measurementId: "G-WS2ERRGGN0"
   };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  let db = firebase.firestore();
+  let auth = firebase.auth();
 
-
+  
   //Page index----------------------------------------------------------------------
 
   //Login
-  
-  let auth = firebase.auth();
+
 
   let userInput = document.getElementById('userEmail');
   let pswInput = document.getElementById('userPassword');
@@ -29,13 +31,47 @@ firebase.initializeApp(firebaseConfig);
 
     auth.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(()=>{
 
-      auth.signInWithEmailAndPassword(userEmail, userPassword).then(() =>{
-        window.location.replace('../dashboard.html');
-        }).catch(error=>{
+      auth.signInWithEmailAndPassword(userEmail, userPassword).then(
+        (userLogon) =>{    
+          // a ideia é a mesma, eu pego do colection um documento específico, e pego os dados dele    
+      db.collection("users").doc(userLogon.user.uid).get().then((doc)=>{        
+          let name = doc.data().name;
+          let lastName = doc.data().lastName;
+          let id = doc.data().id;
+          let city = doc.data().city;
+          let dietMenu = doc.data().diet;
+          let email = doc.data().email;
+          let trainingMenu = doc.data().training;
+          let image = doc.data().image;
+         
+        
+
+          localStorage.setItem("user", name);
+          localStorage.setItem("user_ln", lastName);
+          localStorage.setItem("user_id", id);
+          localStorage.setItem("user_city", city);
+          localStorage.setItem("user_diet1", dietMenu[0]);
+          localStorage.setItem("user_diet2", dietMenu[1]);
+          localStorage.setItem("user_diet3", dietMenu[2]);
+          localStorage.setItem("user_diet4", dietMenu[3]);
+          localStorage.setItem("user_diet5", dietMenu[4]);
+          localStorage.setItem("user_diet6", dietMenu[5]);
+          localStorage.setItem("user_email", email);
+          localStorage.setItem("user_trainingA", trainingMenu[0]);
+          localStorage.setItem("user_trainingB", trainingMenu[1]);
+          localStorage.setItem("user_trainingC", trainingMenu[2]);
+          localStorage.setItem("user_trainingD", trainingMenu[3]);
+          localStorage.setItem("user_image", image);
+          
+
+          window.location.replace('../dashboard.html');
+          })  
+      }).catch(error=>{
           alert('Usuário não encontrado!\n Verifique seus dados ou crie um novo usuário :)');
         })
     });
 }
+
 
 //Create User----------------------------------------------------------------------------------------
 
@@ -43,29 +79,45 @@ firebase.initializeApp(firebaseConfig);
 let inputCreateUser = document.getElementById('inputCreateUser');
 let inputCreatePswd = document.getElementById('inputCreatePswd');
 let inputUserName = document.getElementById('inputUserName');
+let inputUserLastN = document.getElementById('inputUserLastName');
+let inputUserCity = document.getElementById('inputUserCity');
+
+
 
 function createUser(){
     let newUserEmail = inputCreateUser.value;
     let newUserPassword = inputCreatePswd.value;
-    
-    auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword).then(
-        () => {
-
-          generateData();
-        
-        alert('Usuário criado com Sucesso! :)')
-        setTimeout(() => {
-          window.location.replace('../index.html');
-      }, 1000);}).catch(error =>{
-          console.log('Favor, verifique os dados digitados');
-        })
-        
-      }
+    let newUserName = inputUserName.value;
+    let newUserLN = inputUserLastN.value;
+    let newUserCity = inputUserCity.value;
 
 
-      function generateData (){   // <<< working on it
+    auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword).then((data) => {    
+      // esse tem vem com informações do usuário cadastrado    
+      // eu pegaria o id desse valor para poder criar um documento no storage com esse valor.    
+      db.collection("users").doc(data.user.uid).set({        
+          // Perceba que eu crio um documento em um acoleção específica de 
+          // usuários que vai ser setado inicialmente        
+          // apenas com nome, dessa forma você guardará informações no firestore      
+          name: newUserName,    
+          lastName: newUserLN,
+          email: newUserEmail,
+          id: data.user.uid,
+          city: newUserCity,
+          image: 'vazio',
+          training: ['treinoA', 'treinoB', 'treinoC', 'treinoD'],
+          diet: ['Refeição1', 'Refeição2','Refeição3','Refeição4','Refeição5','Refeição6',],
+          });    
+          alert('Usuário criado com Sucesso! :)');
+          setTimeout(() => {
+            window.location.replace('../index.html');
+        }, 1000);
+      })  .catch((err) => {    
+          //   caso tiver algum erro...    
+         alert ('Favor, verifique os dados digitados');
+      });
 
-      }
+    }
 
 
 
--- script/dashboard.js
+++ script/dashboard.js
@@ -3,14 +3,14 @@ let op2 = document.getElementById('op2');
 let op3 = document.getElementById('op3');
 let op4 = document.getElementById('op4');
 let btns_back = document.querySelectorAll('.back');
-let training = document.getElementsByClassName('training')[0];
+let menuTraining = document.getElementsByClassName('training')[0];
 let profile = document.getElementsByClassName('profile')[0];
 let diet = document.getElementsByClassName('diet')[0];
 
 
 //option training
 op1.addEventListener('click',()=>{
-    training.style.display='flex';
+    menuTraining.style.display='flex';
 })
 
 //option profile
@@ -27,7 +27,7 @@ op3.addEventListener('click',()=>{
 btns_back.forEach((button)=>{button.onclick = backDashboard});
 
 function backDashboard(){                            //Clean all pop-ups
-   training.style.display = 'none';
+   menuTraining.style.display = 'none';
    profile.style.display = 'none';
    diet.style.display = 'none';
 }
@@ -42,3 +42,79 @@ op4.addEventListener('click',()=>{
     logout();
     }
 });
+
+//Session login
+
+let userSession = document.getElementById('userSession');
+let user = localStorage.getItem("user");
+
+userSession.innerHTML = user;
+
+//Loading DATA...
+
+//menu
+// menu training
+let menu1 = document.getElementById('menu1');
+
+let treinoA = document.getElementById('treinoA');
+let treinoB = document.getElementById('treinoB');
+let treinoC = document.getElementById('treinoC');
+let treinoD = document.getElementById('treinoD');
+
+let trnA = localStorage.getItem("user_trainingA");
+let trnB = localStorage.getItem("user_trainingB");
+let trnC = localStorage.getItem("user_trainingC");
+let trnD = localStorage.getItem("user_trainingD");
+
+treinoA.innerText = trnA;
+treinoB.innerText = trnB;
+treinoC.innerText = trnC;
+treinoD.innerText = trnD;
+
+
+// menu profile
+let menu2 = document.getElementById('menu2');
+
+let name_user = document.getElementById('name_user')
+let Last_name = document.getElementById('last_name')
+let id_user = document.getElementById('id_user')
+let city = document.getElementById('city')
+let email_user = document.getElementById('email_user')
+
+let nm = localStorage.getItem("user");
+let ln = localStorage.getItem("user_ln");
+let id = localStorage.getItem("user_id");
+let ct = localStorage.getItem("user_city");
+let em = localStorage.getItem("user_email");
+let img = localStorage.getItem("user_image");
+
+name_user.innerText = nm;
+Last_name.innerText = ln;
+id_user.innerText = id;
+city.innerText = ct;
+email_user.innerText = em;
+
+
+// menu diet
+let menu3 = document.getElementById('menu3');
+
+let ref1 = document.getElementById('refeicao1');
+let ref2 = document.getElementById('refeicao2');
+let ref3 = document.getElementById('refeicao3');
+let ref4 = document.getElementById('refeicao4');
+let ref5 = document.getElementById('refeicao5');
+let ref6 = document.getElementById('refeicao6');
+
+let dietItem1 = localStorage.getItem("user_diet1");
+let dietItem2 = localStorage.getItem("user_diet2");
+let dietItem3 = localStorage.getItem("user_diet3");
+let dietItem4 = localStorage.getItem("user_diet4");
+let dietItem5 = localStorage.getItem("user_diet5");
+let dietItem6 = localStorage.getItem("user_diet6");
+
+ref1.innerText = dietItem1; 
+ref2.innerText = dietItem2; 
+ref3.innerText = dietItem3; 
+ref4.innerText = dietItem4; 
+ref5.innerText = dietItem5; 
+ref6.innerText = dietItem6; 
