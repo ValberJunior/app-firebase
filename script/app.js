d23d2e81175153001a2b1562347eb64a0301ba39
ValberJunior <valberjr1992@gmail.com>
2021-09-14 13:55:18 -0300

    update

--- script/app.js
+++ script/app.js
@@ -36,7 +36,8 @@ const firebaseConfig = {
       auth.signInWithEmailAndPassword(userEmail, userPassword).then(
         (userLogon) =>{      
       db.collection("users").doc(userLogon.user.uid).get().then((doc)=>{   
-        alert('Bem-Vindo(a) '+ doc.data().name + ' :)')    
+        alert('Bem-Vindo(a) '+ doc.data().name + ' :)')   
+        load(); 
         window.location.replace('../dashboard.html');
           })  
       }).catch(error=>{
@@ -197,7 +198,8 @@ firebase.auth().onAuthStateChanged(function (user) {
                 }).catch(err=> alert('Erro ao atualizar dados', err));
               }
 
-                  alert("Perfil atualizado");
+                load();
+                  
      
       }    
 
@@ -210,3 +212,78 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 
 
+  //Loading DATA...
+
+  function load(){   
+    //check if the user is logged in...
+    
+    firebase.auth().onAuthStateChanged(function (user) {  
+      if (user) {    
+      db.collection("users").doc(user.uid).get().then((snapshot) => {   
+          
+      //Session login
+      
+      let userSession = document.getElementById('userSession');
+      let user = `Olá ${snapshot.data().name} <i class="fas fa-smile"></i>`;
+      
+      userSession.innerHTML = user;
+      
+                      
+      //menu
+      // menu training
+      let menu1 = document.getElementById('menu1');
+      
+      let treinoA = document.getElementById('treinoA');
+      let treinoB = document.getElementById('treinoB');
+      let treinoC = document.getElementById('treinoC');
+      let treinoD = document.getElementById('treinoD');
+      
+      treinoA.innerText = snapshot.data().training[0];
+      treinoB.innerText = snapshot.data().training[1];
+      treinoC.innerText = snapshot.data().training[2];
+      treinoD.innerText = snapshot.data().training[3];
+      
+      
+      // menu profile
+      let menu2 = document.getElementById('menu2');
+      
+      let profile_picture = document.getElementById('profile_picture')
+      let name_user = document.getElementById('name_user')
+      let Last_name = document.getElementById('last_name')
+      let id_user = document.getElementById('id_user')
+      let city = document.getElementById('city')
+      let email_user = document.getElementById('email_user')
+      
+      
+      profile_picture.src = snapshot.data().image;
+      name_user.innerText = snapshot.data().name;
+      Last_name.innerText = snapshot.data().lastName;
+      id_user.innerText = snapshot.data().id;
+      city.innerText = snapshot.data().city;
+      email_user.innerText = snapshot.data().email;
+      
+      
+      // menu diet
+      let menu3 = document.getElementById('menu3');
+      
+      let ref1 = document.getElementById('refeicao1');
+      let ref2 = document.getElementById('refeicao2');
+      let ref3 = document.getElementById('refeicao3');
+      let ref4 = document.getElementById('refeicao4');
+      let ref5 = document.getElementById('refeicao5');
+      let ref6 = document.getElementById('refeicao6');
+      
+      
+      
+      ref1.innerText = snapshot.data().diet[0]; 
+      ref2.innerText = snapshot.data().diet[1]; 
+      ref3.innerText = snapshot.data().diet[2]; 
+      ref4.innerText = snapshot.data().diet[3]; 
+      ref5.innerText = snapshot.data().diet[4]; 
+      ref6.innerText = snapshot.data().diet[5]; 
+    
+                             
+      });  
+      }});
+    
+   }
--- script/dashboard.js
+++ script/dashboard.js
@@ -1,4 +1,6 @@
-document.onload = load();
+ document.onload = load();
+
+
 
 
 let op1 = document.getElementById('op1');
@@ -16,16 +18,19 @@ let diet = document.getElementsByClassName('diet')[0];
 //option training
 op1.addEventListener('click',()=>{
     menuTraining.style.display='grid';
+    
 })
 
 //option profile
 op2.addEventListener('click',()=>{
     profile.style.display='grid';
+    
 })
 
 //option training
 op3.addEventListener('click',()=>{
     diet.style.display='grid';
+    
 })
 
 //Back to menu dashboard;
@@ -50,81 +55,6 @@ op4.addEventListener('click',()=>{
 });
 
 
-    //Loading DATA...
-
- function load(){   
-  //check if the user is logged in...
-  
-  firebase.auth().onAuthStateChanged(function (user) {  
-    if (user) {    
-    db.collection("users").doc(user.uid).get().then((snapshot) => {   
-        
-    //Session login
-    
-    let userSession = document.getElementById('userSession');
-    let user = `Olá ${snapshot.data().name} <i class="fas fa-smile"></i>`;
-    
-    userSession.innerHTML = user;
-    
-                    
-    //menu
-    // menu training
-    let menu1 = document.getElementById('menu1');
-    
-    let treinoA = document.getElementById('treinoA');
-    let treinoB = document.getElementById('treinoB');
-    let treinoC = document.getElementById('treinoC');
-    let treinoD = document.getElementById('treinoD');
-    
-    treinoA.innerText = snapshot.data().training[0];
-    treinoB.innerText = snapshot.data().training[1];
-    treinoC.innerText = snapshot.data().training[2];
-    treinoD.innerText = snapshot.data().training[3];
-    
-    
-    // menu profile
-    let menu2 = document.getElementById('menu2');
-    
-    let profile_picture = document.getElementById('profile_picture')
-    let name_user = document.getElementById('name_user')
-    let Last_name = document.getElementById('last_name')
-    let id_user = document.getElementById('id_user')
-    let city = document.getElementById('city')
-    let email_user = document.getElementById('email_user')
-    
-    
-    profile_picture.src = snapshot.data().image;
-    name_user.innerText = snapshot.data().name;
-    Last_name.innerText = snapshot.data().lastName;
-    id_user.innerText = snapshot.data().id;
-    city.innerText = snapshot.data().city;
-    email_user.innerText = snapshot.data().email;
-    
-    
-    // menu diet
-    let menu3 = document.getElementById('menu3');
-    
-    let ref1 = document.getElementById('refeicao1');
-    let ref2 = document.getElementById('refeicao2');
-    let ref3 = document.getElementById('refeicao3');
-    let ref4 = document.getElementById('refeicao4');
-    let ref5 = document.getElementById('refeicao5');
-    let ref6 = document.getElementById('refeicao6');
-    
-    
-    
-    ref1.innerText = snapshot.data().diet[0]; 
-    ref2.innerText = snapshot.data().diet[1]; 
-    ref3.innerText = snapshot.data().diet[2]; 
-    ref4.innerText = snapshot.data().diet[3]; 
-    ref5.innerText = snapshot.data().diet[4]; 
-    ref6.innerText = snapshot.data().diet[5]; 
-  
-                           
-    });  
-    }});
-  
- }
 //edit profile
 
 btn_edit.addEventListener('click',()=>{
@@ -137,9 +67,10 @@ let btn_save = document.getElementById('save');
 
 btn_save.addEventListener('click', ()=>{
     let confirmation = window.confirm('Deseja Salvar as alterações?')
-
+    let action = false;
     if(confirmation){
         upgradeprofile();
+
     } else {
         window.location.replace('../dashboard.html');
     }
